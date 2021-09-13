c4bdf23387a604f85a63a7053f65f6056cc6ba8f
millyguitron <millyguitron@outlook.com>
2021-09-12 19:19:42 -0700

    Allowing GET requests from anywhere

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,16 @@
 class LocationsController < ApplicationController
   def show
+    set_headers
     origin = {lat: params[:lat], lon: params[:lon]} if params[:lat].present? && params[:lon].present?
     render json: Conductors::NearbyTransit.new(origin: origin).run
+  end
+
+  private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
